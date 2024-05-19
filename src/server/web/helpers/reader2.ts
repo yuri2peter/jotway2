fdff0d6635230aa56596eda033bcbb8dafc0b5cd
yuri2 <yuri2peter@qq.com>
2024-05-19 22:10:28 +0800

    opt browser options

--- src/server/web/helpers/reader2.ts
+++ src/server/web/helpers/reader2.ts
@@ -1,22 +1,40 @@
 import path from 'path';
-import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import puppeteer from 'puppeteer';
 import { generateContentWithImage } from './ai';
-// import { sleep } from 'src/common/utils/time';
+import { sleep } from 'src/common/utils/time';
 import { runtimeUploadsPath } from 'src/common/paths.app';
 import { nanoid } from 'nanoid';
 import { UPLOADS_URL_PREFIX } from 'src/common/config';
-
-puppeteer.use(StealthPlugin());
 
 // parse url into title, description, icon, content
 export async function urlParser(url: string) {
   // console.log(puppeteer.executablePath());
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+  // avoid robot detect
+  await page.setUserAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+  );
+  await page.evaluateOnNewDocument(() => {
+    // Disable WebGL
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => false,
+    });
+    // Modify WebRTC
+    const patchedRTCConfig = {
+      iceServers: [{ urls: 'stun:stun.example.org' }],
+    };
+    Object.defineProperty(window, 'RTCConfiguration', {
+      writable: false,
+      value: patchedRTCConfig,
+    });
+  });
+  // begin to read
   await page.setViewport({ width: 1440, height: 900 });
-  // await page.goto(url);
-  await page.waitForResponse(url, { timeout: 3000 });
+  await page.goto(url, { timeout: 8000 }).catch(() => {
+    console.warn(`URL ${url} too slow.`);
+  });
+  await sleep(1000);
   const screenshotFileName = `${nanoid()}.png`;
   const screenshotPath = path.resolve(runtimeUploadsPath, screenshotFileName);
   await page.screenshot({
@@ -29,11 +47,14 @@ export async function urlParser(url: string) {
       return 'No description.';
     });
   await browser.close();
-  const summary = await generateContentWithImage(
-    `
+  const prompt1 = `
 Summarize this webpage in two sentence based on the following information and screenshots of the webpage. 
-The summary should use the same language as the information.description's value.
-Information:` +
+The summary must use the same language as the page's content.
+Information:`;
+  const prompt2 = `
+使用如下信息和网页截图来生成这个网页的摘要（不超过两句话）。必须使用和网页内容一样的语种。信息：`;
+  const summary = await generateContentWithImage(
+    prompt2 +
       JSON.stringify({
         title,
         description,
